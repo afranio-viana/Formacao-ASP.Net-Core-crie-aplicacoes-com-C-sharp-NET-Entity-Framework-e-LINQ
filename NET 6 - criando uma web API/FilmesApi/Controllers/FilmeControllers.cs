f4e993d80f2a451fee2264782065f389e1d937a3
afranio-viana <afranio.esquerdo.viana@gmail.com>
2023-07-31 11:53:33 -0400

    Adicionando o início do Post

---
+++ NET 6 - criando uma web API/FilmesApi/Controllers/FilmeControllers.cs
@@ -0,0 +1,22 @@
+namespace FilmesApi.Controllers;
+
+/*Importanto o padrão Mvc para APi*/
+using Microsoft.AspNetCore.Mvc;
+using FilmesApi.Models;
+
+/*Indicando que é um controlador*/
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine($"{filme.Titulo}");
+    }
+
+}
